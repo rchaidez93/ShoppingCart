34868b13fff202b2c12c8951001ee047676e865c
rchaidez93 <rchaidez93@gmail.com>
2017-07-07 00:38:05 -0500

    changeset to header and footer

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -1,3 +1,6 @@
+<?php
+include_once 'assets/js/checkout.js';
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -6,6 +9,15 @@
         <link href="<?php echo base_url('assets/css/footer.css'); ?>" rel="stylesheet" type="text/css" media="screen" />
     </head>
     <body>
-    <div class ='header-site' style = "margin-left: 20%;"></div>
+        <div id ='wrapper'> 
+            <div class ='header-site' style = "margin-left: 20%;"></div>
+            <div class = 'checkout-box' >
+                <div class ="checkout">
+                    
+                </div>
+            </div>
+        </div>
+        
+    
     
 
