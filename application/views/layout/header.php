2034cdfb28dab44733ebc644340f9057923a3d01
rchaidez93 <rchaidez93@gmail.com>
2018-02-03 13:28:58 -0600

    changed name

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -4,7 +4,7 @@ include_once 'assets/js/checkout.js';
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Richard's Store</title>
+        <title>Richard and Amber's Store</title>
         <link href="<?php echo base_url('assets/css/header.css'); ?>" rel="stylesheet" type="text/css" media="screen" />
         <link href="<?php echo base_url('assets/css/footer.css'); ?>" rel="stylesheet" type="text/css" media="screen" />
     </head>
